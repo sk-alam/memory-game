0c6f0503d3698deb681827c6f2047af2297f48b0
Sk Alam <alam.skiftekher@gmail.com>
2021-10-13 09:32:33 +1000

    fixed constant

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://picsum.photos/v2/list?page=10&limit=3';
+export const SERVER_URL = 'https://picsum.photos/v2/list?page=10&limit=15';
 export const CARD_FACE_IMAGE_URL = 'https://picsum.photos/id';
 
 export const STATE = {
